86319bd84445b103c98d89623e5024d14b757843
Guilherme Guerreiro de Oliveira <155542954+GuiGuerreiroo@users.noreply.github.com>
2026-04-20 21:41:03 -0300

    feat: implement the create user route with the new logic that create the user in cognito and also in dynamoDB

--- src/modules/create_user/app/create_user_controller.py
+++ src/modules/create_user/app/create_user_controller.py
@@ -1,49 +1,92 @@
+from src.shared.domain.enums.role_enum import ROLE
 from src.shared.helpers.external_interfaces.external_interface import IResponse, IRequest
-from .create_user_usecase import CreateUserUsecase
-from .create_user_viewmodel import CreateUserViewmodel
+from .create_user_usecase import CreateUserUseCase
 from src.shared.helpers.errors.controller_errors import MissingParameters, WrongTypeParameter
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import NoItemsFound
-from src.shared.helpers.external_interfaces.http_codes import OK, NotFound, BadRequest, InternalServerError, Created
-
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, DuplicatedItem, UnconfirmedUserError
+from src.shared.helpers.external_interfaces.http_codes import OK, NotFound, BadRequest, InternalServerError, Conflict
 
 class CreateUserController:
 
-    def __init__(self, usecase: CreateUserUsecase):
-        self.CreateUserUsecase = usecase
+    def __init__(self, usecase: CreateUserUseCase):
+        self.CreateUserUseCase = usecase
 
     def __call__(self, request: IRequest) -> IResponse:
         try:
-            if request.data.get('name') is None:
-                raise MissingParameters('name')
-            if request.data.get('email') is None:
+            email= request.data.get('email')
+
+            if email is None:
                 raise MissingParameters('email')
 
-            user = self.CreateUserUsecase(
-                name=request.data.get('name'),
-                email=request.data.get('email')
+            if not isinstance(email, str):
+                raise WrongTypeParameter('email', 'str', f"{type(email)}")
+            
+            name= request.data.get('name')
+
+            if name is None:
+                raise MissingParameters('name')
+
+            if not isinstance(name, str):
+                raise WrongTypeParameter('name', 'str', f"{type(name)}")
+
+            password= request.data.get('password')
+
+            if password is None:
+                raise MissingParameters('password')
+
+            if not isinstance(password, str):
+                raise WrongTypeParameter('password', 'str', f"{type(password)}")
+
+            role_str= request.data.get('role')
+
+            if role_str is None: 
+                raise MissingParameters('role')
+
+            try:
+                role_enum= ROLE(role_str)
+
+            except ValueError:
+                raise WrongTypeParameter("role", f"one of {[role.name for role in ROLE]}", role_str
+                )
+
+            height= request.data.get('height')
+
+            if height:
+                if not isinstance(height, float):
+                    raise WrongTypeParameter('height', 'float', type(height))
+
+            user= self.CreateUserUseCase(
+                email=email,
+                name=name,
+                password=password,
+                role=role_enum,
+                height=height
             )
 
-            viewmodel = CreateUserViewmodel(user)
+            viewmodel = {
+                'user': user.model_dump(mode='json'),
+                'message': 'User successfully created'
+            }
 
-            return Created(viewmodel.to_dict())
+            response = OK(viewmodel)
+            
+            return response
 
         except NoItemsFound as err:
-
             return NotFound(body=err.message)
 
         except MissingParameters as err:
-
             return BadRequest(body=err.message)
 
         except WrongTypeParameter as err:
-
             return BadRequest(body=err.message)
 
         except EntityError as err:
-
             return BadRequest(body=err.message)
 
-        except Exception as err:
+        except (DuplicatedItem, UnconfirmedUserError) as err:
+            return Conflict(body=err.message)
 
-            return InternalServerError(body=err.args[0])
+        except Exception as err:
+            return InternalServerError(body=str(err))
--- src/modules/create_user/app/create_user_presenter.py
+++ src/modules/create_user/app/create_user_presenter.py
@@ -1,21 +1,18 @@
 from .create_user_controller import CreateUserController
-from .create_user_usecase import CreateUserUsecase
+from .create_user_usecase import CreateUserUseCase
 from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 
-repo = Environments.get_user_repo()()
-usecase = CreateUserUsecase(repo)
+repo = Environments.get_user_repo()
+usecase = CreateUserUseCase(repo)
 controller = CreateUserController(usecase)
 
 def lambda_handler(event, context):
+    http_request= LambdaHttpRequest(data=event)
 
-    from pprint import pprint
+    response= controller(http_request)
 
-    pprint(event)
-
-    httpRequest = LambdaHttpRequest(data=event)
-    response = controller(httpRequest)
-    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
-
-    return httpResponse.toDict()
+    http_response= LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    
+    return http_response.toDict()
 
