8d0dc6dd9826175547bc29a340865b8bb85405ca
Guilherme Guerreiro de Oliveira <155542954+GuiGuerreiroo@users.noreply.github.com>
2026-05-04 15:39:58 -0300

    fix: update to the first version of update_user route

--- tests/modules/update_user/app/test_update_user_presenter.py
+++ tests/modules/update_user/app/test_update_user_presenter.py
@@ -1,65 +1,62 @@
-# import json
+import json
 
-# from src.modules.update_user.app.update_user_presenter import lambda_handler
-
-
-# class Test_UpdateUserPresenter:
-
-#     def test_update_user(self):
-#         event = {
-#             "version": "2.0",
-#             "routeKey": "$default",
-#             "rawPath": "/my/path",
-#             "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
-#             "cookies": [
-#                 "cookie1",
-#                 "cookie2"
-#             ],
-#             "headers": {
-#                 "header1": "value1",
-#                 "header2": "value1,value2"
-#             },
-#             "queryStringParameters": {
-#                 "parameter1": "1"
-#             },
-#             "requestContext": {
-#                 "accountId": "123456789012",
-#                 "apiId": "<urlid>",
-#                 "authentication": None,
-#                 "authorizer": {
-#                     "iam": {
-#                         "accessKey": "AKIA...",
-#                         "accountId": "111122223333",
-#                         "callerId": "AIDA...",
-#                         "cognitoIdentity": None,
-#                         "principalOrgId": None,
-#                         "userArn": "arn:aws:iam::111122223333:user/example-user",
-#                         "userId": "AIDA..."
-#                     }
-#                 },
-#                 "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
-#                 "domainPrefix": "<url-id>",
-#                 "external_interfaces": {
-#                     "method": "POST",
-#                     "path": "/my/path",
-#                     "protocol": "HTTP/1.1",
-#                     "sourceIp": "123.123.123.123",
-#                     "userAgent": "agent"
-#                 },
-#                 "requestId": "id",
-#                 "routeKey": "$default",
-#                 "stage": "$default",
-#                 "time": "12/Mar/2020:19:03:58 +0000",
-#                 "timeEpoch": 1583348638390
-#             },
-#             "body": '{"user_id": "1",  "new_name": "João Soller"}',
-#             "pathParameters": None,
-#             "isBase64Encoded": None,
-#             "stageVariables": None
-#         }
-
-#         response = lambda_handler(event, None)
+from src.modules.update_user.app.update_user_presenter import lambda_handler
 
 
-#         assert response["statusCode"] == 200
-#         assert json.loads(response["body"])['name'] == 'João Soller'
+class Test_UpdateUserPresenter:
+
+    def test_update_user(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "queryStringParameters": {
+                "parameter1": "1"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "claims": {
+                        "name": "Guilherme",
+                        "email": "25.00178-5@maua.br",
+                        "sub": "550e8400-e29b-41d4-a716-446655440002",
+                        "custom:role": "USER"
+                    }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "external_interfaces": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": '{"new_name": "João Soller"}',
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+
+        response = lambda_handler(event, None)
+
+        assert response["statusCode"] == 200
+        assert json.loads(response["body"])['user']['name'] == 'João Soller'
+        assert json.loads(response["body"])['message'] == 'the user was updated successfully'
